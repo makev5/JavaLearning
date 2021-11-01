d9972f6cf8c1e9c7b78afbddd83da01369442023
make <702164551@qq.com>
2021-11-01 13:47:54 +0800

    first commit

---
+++ homework/Student.java
@@ -0,0 +1,34 @@
+public class Student {
+    private String name;            // 姓名
+    private String studentNumber;   // 学号
+    private int age;                // 年龄
+    private String sex;             // 性别
+
+    public void show(){
+        System.out.println("大家好，我是"+ getName() + ",学号是：" + getStudentNumber() + ",性别：" + getSex() + ",今年" + getAge() +"岁。");
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+    public void setStudentNumber(String studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getSex() {
+        return sex;
+    }
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
---
+++ homework/StudentDemo.java
@@ -0,0 +1,10 @@
+public class StudentDemo {
+    public static void main(String[] args) {
+        Student stu = new Student();
+        stu.setName("马科");
+        stu.setStudentNumber("21631923115");
+        stu.setSex("男");
+        stu.setAge(22);
+        stu.show();
+    }
+}
